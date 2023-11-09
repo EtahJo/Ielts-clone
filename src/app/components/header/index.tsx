6bd71fe981ef49316202c0837cbdf2bf396bfdb7
etah <arrahetah23@gmail.com>
2023-11-09 18:45:37 +0100

    first commit

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="bg-red-600 px-96 flex justify-end">
+      <div className="bg-[rgba(0,0,0,0.5)] p-3 w-2/12 hover:bg-primaryColor cursor-pointer ">
+        <p className="text-white text-center">Test Taker Portal</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
